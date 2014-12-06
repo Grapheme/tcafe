09156a729066451b6f0a7fa366e22b9519a31352
etomarat <etomarat@gmail.com>
2014-12-06 22:45:30 +0300

    Blocks on main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,11 @@
 $(function() {
+  
   function _log(msg) {
     console.log(msg);
-  }
+  };
+  
   $('.menu-wrapper .menu-btn').on('click', function(){
-    $(this).closest('.menu-wrapper').toggleClass('open')
+    $(this).closest('.menu-wrapper').toggleClass('open');
   });
+  
 });
