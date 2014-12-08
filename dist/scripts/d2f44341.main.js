247a4a40ca63588f6f527e5e60f8729840ac76cc
etomarat <etomarat@gmail.com>
2014-12-08 18:07:40 +0300

    build

---
+++ dist/scripts/d2f44341.main.js
@@ -0,0 +1,44 @@
+window._TCAFE_ = {};
+
+function _log(msg) {
+  console.log(msg);
+};
+
+_TCAFE_.setYear = function(){
+  var now = new Date();
+  var year = now.getFullYear();
+  $('#footer .year').text(year);
+}
+
+$(function() {
+  _TCAFE_.setYear();
+});
+
+$(function() {
+  
+  _TCAFE_.Menu = {};
+  _TCAFE_.Menu.menu_is_open = false;
+  _TCAFE_.Menu.$menu_wrapper = $('.menu-wrapper');
+  _TCAFE_.Menu.$menu_btn = _TCAFE_.Menu.$menu_wrapper.find('.menu-btn');
+  
+  _TCAFE_.Menu.menuOpener = function() {
+    if (_TCAFE_.Menu.menu_is_open) {
+      _TCAFE_.Menu.$menu_wrapper.addClass('open');
+    } else {
+      _TCAFE_.Menu.$menu_wrapper.removeClass('open');
+    };
+  };
+  
+  $('html').on('click', _TCAFE_.Menu.$menu_btn, function(){
+    _TCAFE_.Menu.menu_is_open = !_TCAFE_.Menu.menu_is_open;
+    _TCAFE_.Menu.menuOpener();
+  });
+  
+  $('html').click(function(e){
+    if (!$(e.target).closest('.menu-wrapper').size()) {
+      _TCAFE_.Menu.menu_is_open = false;
+      _TCAFE_.Menu.menuOpener();
+    };
+  });
+  
+});
