434f9e672e113346dbf04c0f0bb7eb1a544a7ba2
etomarat <etomarat@gmail.com>
2014-12-15 19:46:47 +0300

    review and states
    final state works well only if POST is correct.

---
+++ app/scripts/reviews.js
@@ -0,0 +1,40 @@
+$(function() {
+  
+  $('.unit.form-holder .title-wrapper').click(function(){
+    $('.unit.form-holder').addClass('open');
+    $('.unit.form-holder').find('form').slideDown();
+  });
+  
+  $("form.review-form").validate({
+    rules: {
+      name: 'required',
+      email: {
+        required: true,
+        email: true
+      },
+      review: 'required'
+    },
+    messages: {
+      name: 'Обязательное поле',
+      email: {
+        required: 'Обязательное поле',
+        email: 'Неверный формат. Попробуйте еще'
+      },
+      review: 'Обязательное поле'
+    },
+    submitHandler: function(form) {
+      var _url = $(form).attr('action'),
+          _data = $(form).serialize(),
+          _method = $(form).attr('method')||'POST';
+      $.ajax({
+        type: _method,
+        url: _url,
+        data: _data,
+        success: function(data) {
+          $('.unit.form-holder .final').fadeIn();
+        }
+      })
+    }
+  });
+
+});
