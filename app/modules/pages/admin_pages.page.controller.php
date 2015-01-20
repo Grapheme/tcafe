ab975ee6c4bce620bb4194b7776616af3e57309e
sngrl <reserved@mail.ru>
2015-01-20 15:52:22 +0300

    fix

--- app/modules/pages/admin_pages.page.controller.php
+++ app/modules/pages/admin_pages.page.controller.php
@@ -492,6 +492,7 @@ class AdminPagesPageController extends BaseController {
 
         #Helper::dd($this->templates(__DIR__, '/views/tpl_block'));
 
+        $templates = array();
         foreach ($this->templates(__DIR__, '/views/tpl_block') as $template)
             @$templates[$template] = $template;
 
