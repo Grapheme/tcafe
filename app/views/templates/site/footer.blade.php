274b4696952ef719b4a86ac5aa359abdf73a1e98
sngrl <reserved@mail.ru>
2015-06-02 12:59:46 +0300

    metrika

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -9,3 +9,32 @@
     <div class="center"><a href="http://www.facebook.com/cafeTcafe" target="_blank">f</a><a href="https://vk.com/tcafe" target="_blank">v</a><a href="http://instagram.com/tcafekrasnodar/" target="_blank">i</a></div>
     <div class="right">Сделано в <a href="http://grapheme.ru/" target="_blank">ГРАФЕМА</a></div>
 </div>
+
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter30656037 = new Ya.Metrika({id:30656037,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/30656037" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
