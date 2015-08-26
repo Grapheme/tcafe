8fc8006b3f7d175964179a4a531261a2c077a4ea
sngrl <reserved@mail.ru>
2015-08-26 12:04:03 +0300

    fix

--- app/views/templates/site/index.blade.php
+++ app/views/templates/site/index.blade.php
@@ -9,7 +9,8 @@
 $bg = Dic::valueBySlugs('options', 'background_mainpage');
 #Helper::tad($bg);
 $mainpage_banner_url = Dic::valueBySlugs('options', 'mainpage_banner_url');
-#echo '<!--'; Helper::ta($mainpage_banner_url); echo '-->';
+echo '<!--'; Helper::ta($mainpage_banner_url); echo '-->';
+echo '<!--'; Helper::ta(preg_match('~\.[a-zA-Z]{3,4}$~is', $mainpage_banner_url->name)); echo '-->';
 ?>
 
 
